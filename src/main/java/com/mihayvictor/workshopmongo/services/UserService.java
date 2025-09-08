27103444061d9f0213e7b40ea913268cc9d45e9a
mihayvictor <m.victorcezario@gmail.com>
2025-09-08 20:16:12 -0300

    Inserção de usuário com POST

--- src/main/java/com/mihayvictor/workshopmongo/services/UserService.java
+++ src/main/java/com/mihayvictor/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mihayvictor.workshopmongo.domain.User;
+import com.mihayvictor.workshopmongo.dto.UserDTO;
 import com.mihayvictor.workshopmongo.repository.UserRepository;
 import com.mihayvictor.workshopmongo.services.exception.ObjectNotFondException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFondException("Objeto Não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
